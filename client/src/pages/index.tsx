02fcb18077cf557b67ce90f653870696ee8dca5e
jochoa <jonatan.c.ochoa@gmail.com>
2023-07-04 13:07:35 -0300

    first commit

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from 'next/image' // importar Imagen con Next JS
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -34,14 +34,16 @@ export default function Home() {
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
+        {/* Ejemplo para renderizar imagenes
+        <Image 
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
           priority
-        />
+        /> */}
+        <h1 className={`font-bold text-3xl`}>PF-DESCART</h1>
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
