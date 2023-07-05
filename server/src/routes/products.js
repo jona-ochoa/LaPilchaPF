287db13b11a75d463b128f5ca3e7e4a4444e5054
Juan191101 <ruartejuancruz@gmail.com>
2023-07-05 17:42:36 -0300

    datos desde db.json

--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -1,18 +1,24 @@
 const express = require("express");
-const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 const router = express.Router();
 
+// Ruta para obtener todos los productos
 router.get("/", (req, res) => {
-  axios
-    .get("https://fakestoreapi.com/products")
-    .then((response) => {
-      const products = response.data;
-      res.json({ products });
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).json({ error: "Error al obtener los productos" });
-    });
+  // Ruta completa al archivo db.json
+  const filePath = path.join(__dirname, "../api/db.json");
+
+  // Leer el archivo db.json
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Error al leer el archivo" });
+    }
+
+    const jsonData = JSON.parse(data);
+    const products = jsonData.products;
+    res.json({ products });
+  });
 });
 
 module.exports = router;
