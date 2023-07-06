13ca0408b56a94f04312571549f3eabdf895256a
Juan191101 <ruartejuancruz@gmail.com>
2023-07-06 18:53:14 -0300

    rutas product en base de datos

--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -1,81 +1,65 @@
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
 const router = express.Router();
+const products = require("../models/products");
 
 // Ruta para obtener todos los productos
 router.get("/", (req, res) => {
-  const filePath = path.join(__dirname, "../api/db.json");
-
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: "Error al leer el archivo" });
-    }
-
-    const jsonData = JSON.parse(data);
-    const products = jsonData.products;
-    res.json({ products });
-  });
+  products
+    .find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
 });
 
-//ruta para buscar por id
+// Ruta para buscar productos por nombre
+router.get("/search", (req, res) => {
+  const keyword = req.query.keyword;
+
+  products
+    .find({ $text: { $search: keyword } })
+    .then((filteredProducts) => res.json({ products: filteredProducts }))
+    .catch((error) => res.status(500).json({ error: "Error al obtener los productos" }));
+});
+
+// Ruta para buscar por id
 router.get("/:id", (req, res) => {
-  const productId = parseInt(req.params.id);
+  const productId = req.params.id;
+
+  products
+    .findById(productId)
+    .then((product) => {
+      if (!product) {
+        return res.status(404).json({ error: "Producto no encontrado" });
+      }
+      res.json({ product });
+    })
+    .catch((error) => res.status(500).json({ error: `Error al obtener el producto con id ${productId}` }));
+});
+
+module.exports = router;
+
+/* router.get("/search", (req, res) => {
+  const keyword = req.query.keyword;
 
   // Leer el archivo db.json
   fs.readFile(path.join(__dirname, "../api/db.json"), "utf8", (err, data) => {
     if (err) {
       console.error(err);
-      return res.status(500).json({ error: "Error al obtener el producto" });
+      return res.status(500).json({ error: "Error al obtener los productos" });
     }
 
     try {
       const db = JSON.parse(data);
       const products = db.products;
 
-      // Buscar el producto por su ID
-      const product = products.find((product) => product.id === productId);
+      // Filtrar los productos que contengan la palabra clave en el título
+      const filteredProducts = products.filter((product) =>
+        product.title.toLowerCase().includes(keyword.toLowerCase())
+      );
 
-      if (!product) {
-        return res.status(404).json({ error: "Producto no encontrado" });
-      }
-
-      res.json({ product });
+      res.json({ products: filteredProducts });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: "Error al obtener el producto" });
+      res.status(500).json({ error: "Error al obtener los productos" });
     }
   });
-});
-
-// Ruta para buscar productos por nombre
-router.get("/search", (req, res) => {
-    const keyword = req.query.keyword;
-  
-    // Leer el archivo db.json
-    fs.readFile(path.join(__dirname, "../api/db.json"), "utf8", (err, data) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: "Error al obtener los productos" });
-      }
-  
-      try {
-        const db = JSON.parse(data);
-        const products = db.products;
-  
-        // Filtrar los productos que contengan la palabra clave en el título
-        const filteredProducts = products.filter((product) =>
-          product.title.toLowerCase().includes(keyword.toLowerCase())
-        );
-  
-        res.json({ products: filteredProducts });
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Error al obtener los productos" });
-      }
-    });
-  });
-  
-
-module.exports = router;
+}); */
