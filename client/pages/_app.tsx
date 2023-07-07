1dab4b87d9570739aabec68ab58e6f3520ea3aad
JMHaryBeccarVarela <123906403+JMHaryBeccarVarela@users.noreply.github.com>
2023-07-06 23:06:20 -0300

    comente la importacion del detail en app.tsx

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Detail from '../src/components/ProductDetail/detail';
+/* import Detail from '../src/components/ProductDetail/detail'; */
 import Navbar from '../src/components/Navbar/Navbar';
 import type { AppProps } from 'next/app';
 import '../src/styles/globals.css'; // Importa el archivo CSS global de Tailwind CSS
