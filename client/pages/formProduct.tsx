30a48e63e7340dda7c0fb4e62178a6c96982d621
Malena Valdez <malevaldezok@gmail.com>
2023-07-07 23:10:35 -0300

    se añade el formulario de productos

---
+++ client/pages/formProduct.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProductForm from './../src/components/ProductForm/ProductForm'
+const FormProductPage = () => {
+    return (
+        <div>
+            <div>
+            <ProductForm />
+            </div>
+        </div>
+    )
+}
+
+export default FormProductPage;
