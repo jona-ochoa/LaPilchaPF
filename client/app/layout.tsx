7364d314c256356e5fe53f60ff5881d660282a08
JMHaryBeccarVarela <123906403+JMHaryBeccarVarela@users.noreply.github.com>
2023-07-09 01:22:36 -0300

    layout problems

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Providers } from "../GlobalRedux/provider"
-import { Provider } from 'react-redux'
 
 const inter = Inter({ subsets: ['latin'] })
 
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,23 +1,17 @@
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 import ProductsPage from "./products/page"
-
-
-
-
-
+import RootLayout from "./layout"
 
 export default function HomePage() {
 
-
-
   return (
 <div> 
-
-    <Navbar/>
-    <ProductsPage/>
-    <Footer/>
-
+  <RootLayout>
+    <Navbar />
+    <ProductsPage />
+    <Footer />
+  </RootLayout>
 </div>
   )
 }
