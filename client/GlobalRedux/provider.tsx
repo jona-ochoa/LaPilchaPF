a7a70ea5717bfe2e566aadb2861b058946347ba3
JMHaryBeccarVarela <123906403+JMHaryBeccarVarela@users.noreply.github.com>
2023-07-09 20:54:51 -0300

    para solucionar errores

--- client/GlobalRedux/provider.tsx
+++ client/GlobalRedux/provider.tsx
@@ -1,14 +1,10 @@
 import { Provider } from "react-redux";
 import { store } from "./store";
-import Navbar from "../components/Navbar";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <Provider store={store}>
-      <>
-        <Navbar />
         {children}
-      </>
     </Provider>
   );
 }
--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,15 +1,13 @@
-import Link from "next/link"
+import Link from "next/link";
 
 function Nav() {
   return (
-    <>
     <nav>
-        <Link href='/'>Home</Link>
-        <Link href='/about'>About</Link>
-        <Link href='/product'>Product</Link>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/product">Product</Link>
     </nav>
-    </>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
