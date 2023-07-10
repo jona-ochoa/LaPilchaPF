d787a96fd17773c45c69207b2d2b7ad22791bc74
Malena Valdez <malevaldezok@gmail.com>
2023-07-10 16:35:54 -0300

    modificacion del detail

--- client/components/Detail.tsx
+++ client/components/Detail.tsx
@@ -29,7 +29,7 @@ import { Product } from '../GlobalRedux/api/productsApi';
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 text-red-500" viewBox="0 0 24 24">
               <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
             </svg>
-            <span className="text-gray-600 ml-3">Rating:{rating.rate} Votes Submitted: {rating.count}</span>
+            <span className="text-gray-600 ml-3">Rating:{rating?.rate} Votes Submitted: {rating?.count}</span>
 
               </span>
               <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200">
