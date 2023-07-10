a49b6b01a3454ea731aadf03c16ff8fd304439c7
JMHaryBeccarVarela <123906403+JMHaryBeccarVarela@users.noreply.github.com>
2023-07-09 23:03:52 -0300

    fixed issues

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,20 +6,19 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import ProductsPage from './products/page';
 import RootLayout from './layout';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './globals.css';
 
 const HomePage = () => {
   return (
-    <Router>   
+       
     <div>  
       <Navbar />
-      <Routes>
-        <Route path="/" element={<ProductsPage />} />
-      </Routes>
+      <RootLayout>
+      <ProductsPage />
+      </RootLayout>
       <Footer />
     </div>
-  </Router>
+  
   );
 }
 
