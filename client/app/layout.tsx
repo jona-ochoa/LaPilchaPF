bc56fc45de8751c71e44a04e7c6c425971bc3238
arielbloise <arielnbloise@gmail.com>
2023-07-11 11:18:33 -0300

    Configurado Login con Autorización de Terceros y Navbar

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,6 +2,7 @@
 import {Inter} from "next/font/google"
 import React from 'react';
 import { Providers } from "../GlobalRedux/provider";
+import { SessionProvider } from "next-auth/react";
 import "./globals.css";
 
 const inter = Inter({subsets: ['latin']})
@@ -9,11 +10,13 @@ const inter = Inter({subsets: ['latin']})
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
+      <SessionProvider>
       <Providers>
       <body className={inter.className}>
        {children}
        </body> 
       </Providers>
+      </SessionProvider>
     </html>
       
   );
--- client/next.config.js
+++ client/next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   plugins: ['tailwindcss'],
   reactStrictMode: true,
   swcMinify: true,
-}
+  images:{
+    domains:[
+      'lh3.googleusercontent.com',
+      'avatars.githubusercontent.com'
+    ],
+  },
+};
 
 module.exports = nextConfig
