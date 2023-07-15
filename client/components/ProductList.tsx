d0d1767c3f7e44db5630bfebaf0ccfd6a972b6e0
jochoa <jonatan.c.ochoa@gmail.com>
2023-07-15 17:24:50 -0300

    notificaciones de añadir o eliminar en carrito/favoritos

--- client/components/ProductList.tsx
+++ client/components/ProductList.tsx
@@ -7,9 +7,15 @@ import { addToFavorites, removeFromFavorites } from "../GlobalRedux/features/fav
 import { setProducts } from '../GlobalRedux/features/productsSlice';
 import ProductCard from './ProductCard';
 import { useLocalStorage } from '../hooks/useLocalstorage';
+import toast from 'react-hot-toast'
+import { addToCarrito, removeFromCarrito } from "../GlobalRedux/features/carritoSlice"
+import { BsFillCartCheckFill, BsFillCartDashFill } from 'react-icons/bs'
+import { MdFavorite, MdFavoriteBorder } from 'react-icons/md'
 
 const ProductList: React.FC = () => {
   const [favoriteItems, setFavoriteItems] = useLocalStorage<Product[]>("favoriteItems", []);
+  const [cartItems, setCartItems] = useLocalStorage<Product[]>('cartItems', []);
+
   const dispatch = useDispatch();
   const { data, error, isLoading, isFetching } = useGetProductsQuery(null);
 
@@ -27,15 +33,32 @@ const ProductList: React.FC = () => {
     }
   }, [data, dispatch]);
 
+  const handleCartItem = (item: Product) => {
+    const existingItemCarrito = cartItems.find((_item) => _item._id === item._id);
+
+    if (existingItemCarrito) {
+      dispatch(removeFromCarrito(item._id))
+      const updatedCarrito = cartItems.filter((carrito) => carrito._id !== item._id);
+      setCartItems(updatedCarrito);
+      toast.error('Eliminado del Carrito.')
+    } else {
+      dispatch(addToCarrito(item));
+      setCartItems([...cartItems, item]);
+      toast.success('Agregado al Carrito.')
+    }
+  };
+
   const handleFavorite = (item: Product) => {
     const existingItem = favoriteItems.find((_item) => _item._id === item._id);
     if (existingItem) {
       dispatch(removeFromFavorites(item._id));
       const updatedFavorites = favoriteItems.filter((favorite) => favorite._id !== item._id);
       setFavoriteItems(updatedFavorites);
+      toast.error('Eliminado de Favoritos')
     } else {
       dispatch(addToFavorites(item));
       setFavoriteItems([...favoriteItems, item]);
+      toast.success('Agregado a Favoritos')
     }
   };
 
@@ -71,12 +94,21 @@ const ProductList: React.FC = () => {
         {currentProducts.map((product) => (
           <div key={product._id} className="product-card relative">
             <ProductCard product={product} />
+            <div className="absolute top-2 right-7 flex flex-col bg-gray-200 p-1 rounded-sm">
             <button
               onClick={() => handleFavorite(product)}
-              className="absolute top-2 right-7"
+              className=""
             >
-              {favoriteItems.find((item) => item._id === product._id) ? '❤️' : '🤍'}
+              {favoriteItems.find((item) => item._id === product._id) ? <MdFavorite className="w-[25px] h-[25px] text-red-500 transition-transform" /> : <MdFavoriteBorder className="w-[25px] h-[25px] text-gray-800 transition-transform opacity-75" />}
             </button>
+            <button
+              onClick={() => handleCartItem(product)}
+              className=""
+            >
+              {cartItems.find((item) => item._id === product._id) ? <BsFillCartCheckFill className="w-[25px] h-[25px] text-green-500 transition-transform" /> : <BsFillCartDashFill className="w-[25px] h-[25px] text-gray-800 transition-transform opacity-75" />}
+            </button>
+            </div>
+          
           </div>
         ))}
       </div>
@@ -86,9 +118,8 @@ const ProductList: React.FC = () => {
           <button
             key={index}
             onClick={() => handlePageChange(index + 1)}
-            className={`px-3 py-1 mx-1 ${
-              currentPage === index + 1 ? "bg-gray-600 text-white" : "bg-gray-300 text-gray-700"
-            }`}
+            className={`px-3 py-1 mx-1 ${currentPage === index + 1 ? "bg-gray-600 text-white" : "bg-gray-300 text-gray-700"
+              }`}
           >
             {index + 1}
           </button>
