b87861fb3d8a4e27906ed3b7aa7af6d89bb6a669
JmHaryBeccarVarela <julianhary04@gmail.com>
2023-07-16 19:16:37 -0300

    funcionalidad de searchbar

--- client/GlobalRedux/store.ts
+++ client/GlobalRedux/store.ts
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware, combineReducers } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { useDispatch } from 'react-redux';
 import { productsApi } from './api/productsApi';
@@ -7,21 +7,24 @@ import productsReducer from "./features/productsSlice";
 import userSlice from './features/usersSlice';
 import carritoReducer from "./features/carritoSlice"
 import favoritosReducer from './features/favoritoSlice';
+import searchQueryReducer from './features/searchQuerySlice';
 
+const reducer = combineReducers({
+  [productsApi.reducerPath]: productsApi.reducer,
+  products: productsReducer,
+  items: carritoReducer,
+  favoritos: favoritosReducer,
+  user: userSlice,
+  [usersApi.reducerPath]: usersApi.reducer,
+  searchQuery: searchQueryReducer,
+});
 
 const middleware = getDefaultMiddleware()
-.concat(productsApi.middleware)
-.concat(usersApi.middleware)
+  .concat(productsApi.middleware)
+  .concat(usersApi.middleware);
 
 export const store = configureStore({
-  reducer: {
-    [productsApi.reducerPath]: productsApi.reducer,
-    products: productsReducer,
-    items: carritoReducer,
-    favoritos: favoritosReducer,
-    user: userSlice,
-    [usersApi.reducerPath]: usersApi.reducer
-  },
+  reducer,
   middleware,
   devTools: process.env.NODE_ENV !== 'production',
 });
--- client/components/ProductList.tsx
+++ client/components/ProductList.tsx
@@ -1,23 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState } from '../GlobalRedux/store';
+import { useSelector } from 'react-redux';
+import { useAppDispatch } from "../GlobalRedux/hooks";
 import { Product } from '../GlobalRedux/api/productsApi';
-import { useGetProductsQuery } from '../GlobalRedux/api/productsApi';
+import { useGetProductsByTitleQuery, useGetProductsQuery } from '../GlobalRedux/api/productsApi';
 import { addToFavorites, removeFromFavorites } from "../GlobalRedux/features/favoritoSlice";
 import { setProducts } from '../GlobalRedux/features/productsSlice';
 import ProductCard from './ProductCard';
+import { RootState } from '../GlobalRedux/store';
 import { useLocalStorage } from '../hooks/useLocalstorage';
-import toast from 'react-hot-toast'
-import { addToCarrito, removeFromCarrito } from "../GlobalRedux/features/carritoSlice"
-import { BsFillCartCheckFill, BsFillCartDashFill } from 'react-icons/bs'
-import { MdFavorite, MdFavoriteBorder } from 'react-icons/md'
+import toast from 'react-hot-toast';
+import { addToCarrito, removeFromCarrito } from "../GlobalRedux/features/carritoSlice";
+import { BsFillCartCheckFill, BsFillCartDashFill } from 'react-icons/bs';
+import { MdFavorite, MdFavoriteBorder } from 'react-icons/md';
 
 const ProductList: React.FC = () => {
   const [favoriteItems, setFavoriteItems] = useLocalStorage<Product[]>("favoriteItems", []);
   const [cartItems, setCartItems] = useLocalStorage<Product[]>('cartItems', []);
 
-  const dispatch = useDispatch();
-  const { data, error, isLoading, isFetching } = useGetProductsQuery(null);
+  const dispatch = useAppDispatch();
+  const searchQuery = useSelector((state: RootState) => state.searchQuery.query) as string;
+
+  const { data, error, isLoading, isFetching } = searchQuery
+  ? useGetProductsByTitleQuery({ title: searchQuery })
+  : useGetProductsQuery(null);
 
   const [currentPage, setCurrentPage] = useState(1);
   const productsPerPage = 8;
@@ -27,9 +32,8 @@ const ProductList: React.FC = () => {
   const [selectedCategory, setSelectedCategory] = useState("");
 
   useEffect(() => {
-    console.log("Data:", data);
     if (data) {
-      dispatch(setProducts(data));
+      dispatch(setProducts(data as Product[]));
     }
   }, [data, dispatch]);
 
@@ -37,14 +41,14 @@ const ProductList: React.FC = () => {
     const existingItemCarrito = cartItems.find((_item) => _item._id === item._id);
 
     if (existingItemCarrito) {
-      dispatch(removeFromCarrito(item._id))
+      dispatch(removeFromCarrito(item._id));
       const updatedCarrito = cartItems.filter((carrito) => carrito._id !== item._id);
       setCartItems(updatedCarrito);
-      toast.error('Eliminado del Carrito.')
+      toast.error('Eliminado del Carrito.');
     } else {
       dispatch(addToCarrito(item));
       setCartItems([...cartItems, item]);
-      toast.success('Agregado al Carrito.')
+      toast.success('Agregado al Carrito.');
     }
   };
 
@@ -54,11 +58,11 @@ const ProductList: React.FC = () => {
       dispatch(removeFromFavorites(item._id));
       const updatedFavorites = favoriteItems.filter((favorite) => favorite._id !== item._id);
       setFavoriteItems(updatedFavorites);
-      toast.error('Eliminado de Favoritos')
+      toast.error('Eliminado de Favoritos');
     } else {
       dispatch(addToFavorites(item));
       setFavoriteItems([...favoriteItems, item]);
-      toast.success('Agregado a Favoritos')
+      toast.success('Agregado a Favoritos');
     }
   };
 
@@ -70,19 +74,14 @@ const ProductList: React.FC = () => {
     return <p>Error fetching products</p>;
   }
 
-  const products = data as Product[];
-  const filteredProducts = products.filter((product) => {
-    const price = parseFloat(product.price);
-    const min = minPrice !== "" ? parseFloat(minPrice) : Number.NEGATIVE_INFINITY;
-    const max = maxPrice !== "" ? parseFloat(maxPrice) : Number.POSITIVE_INFINITY;
-    const category = selectedCategory !== "" ? product.category === selectedCategory : true;
-    return price >= min && price <= max && category;
-  });
+  let filteredProducts = data
+  
 
-  const totalPages = Math.ceil(filteredProducts.length / productsPerPage);
+
+  const totalPages = Math.ceil((filteredProducts?.length ?? 0) / productsPerPage);
   const indexOfLastProduct = currentPage * productsPerPage;
   const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-  const currentProducts = filteredProducts.slice(indexOfFirstProduct, indexOfLastProduct);
+  const currentProducts = filteredProducts?.slice(indexOfFirstProduct, indexOfLastProduct);
 
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
@@ -91,24 +90,23 @@ const ProductList: React.FC = () => {
   return (
     <div>
       <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 xl:gap-y-10">
-        {currentProducts.map((product) => (
+        {currentProducts?.map((product) => (
           <div key={product._id} className="product-card relative">
             <ProductCard product={product} />
             <div className="absolute top-2 right-7 flex flex-col bg-gray-200 p-1 rounded-sm">
-            <button
-              onClick={() => handleFavorite(product)}
-              className=""
-            >
-              {favoriteItems.find((item) => item._id === product._id) ? <MdFavorite className="w-[25px] h-[25px] text-red-500 transition-transform" /> : <MdFavoriteBorder className="w-[25px] h-[25px] text-gray-800 transition-transform opacity-75" />}
-            </button>
-            <button
-              onClick={() => handleCartItem(product)}
-              className=""
-            >
-              {cartItems.find((item) => item._id === product._id) ? <BsFillCartCheckFill className="w-[25px] h-[25px] text-green-500 transition-transform" /> : <BsFillCartDashFill className="w-[25px] h-[25px] text-gray-800 transition-transform opacity-75" />}
-            </button>
+              <button
+                onClick={() => handleFavorite(product)}
+                className=""
+              >
+                {favoriteItems.find((item) => item._id === product._id) ? <MdFavorite className="w-[25px] h-[25px] text-red-500 transition-transform" /> : <MdFavoriteBorder className="w-[25px] h-[25px] text-gray-800 transition-transform opacity-75" />}
+              </button>
+              <button
+                onClick={() => handleCartItem(product)}
+                className=""
+              >
+                {cartItems.find((item) => item._id === product._id) ? <BsFillCartCheckFill className="w-[25px] h-[25px] text-green-500 transition-transform" /> : <BsFillCartDashFill className="w-[25px] h-[25px] text-gray-800 transition-transform opacity-75" />}
+              </button>
             </div>
-          
           </div>
         ))}
       </div>
@@ -130,3 +128,6 @@ const ProductList: React.FC = () => {
 };
 
 export default ProductList;
+
+
+
