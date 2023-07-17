9f0224e44bbcc270d3f1af0ce08270bdc91939d8
arielbloise <arielnbloise@gmail.com>
2023-07-17 04:29:24 -0300

    Cloudinary Funcionando

--- client/components/ProductForm.tsx
+++ client/components/ProductForm.tsx
@@ -1,121 +1,146 @@
-"use client"
-import React, {useRef} from 'react';
-import {Formik, Form, Field, ErrorMessage, useFormikContext, FormikHelpers } from 'formik';
+'use client'
+import React, { useRef } from 'react';
+import { Formik, Form, Field, ErrorMessage, useFormikContext, FormikHelpers } from 'formik';
 import * as Yup from 'yup';
-//redux
 import { Product, usePostProductsMutation } from '../GlobalRedux/api/productsApi';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../GlobalRedux/store';
 import { setProducts } from '../GlobalRedux/features/productsSlice';
+import toast from 'react-hot-toast';
 
 interface ProductForm {
-    _id: string;
-    title: string;
-    price: string;
-    description: string;
-    category: string;
-    image: string | File | null;
-    rating: any;
+  _id: string;
+  title: string;
+  price: string;
+  description: string;
+  category: string;
+  image: File | null;
+  rating: any;
 }
 
 const ProductForm: React.FC = () => {
-    const dispatch = useDispatch()
-    const [postProduct, {isLoading, isError}] = usePostProductsMutation();
+  const dispatch = useDispatch();
+  const [postProduct, { isLoading, isError }] = usePostProductsMutation();
 
-    const validationSchema = Yup.object().shape({
-        title: Yup.string().required('Product name is required'),
-        price: Yup.string().required('Product price is required'),
-        description: Yup.string().required('Product description is required'),
-        category: Yup.string().required('The product category is required'),
-        image: Yup.mixed().required('Product image is required').test('fileType', 'The file must be an image', function (value: any) {
-            if(!value) return true;
-            const fileType = value && value.type;
-            const validFileTypes = ['image/jpeg', 'image/png', 'image/jpg'];
-            if(fileType) {
-                return validFileTypes.includes(fileType)
-            }
-            return false;
-        }),
-    });
+  const toaster = useRef(null);
 
-    const handleSubmit = async (values: ProductForm, {resetForm }: FormikHelpers<ProductForm>) => {
-        console.log('submitting form:', values);
-  const { _id, title, price, description, category, image } = values;
-  let productImage: string | null = null;
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required('Product name is required'),
+    price: Yup.string().required('Product price is required'),
+    description: Yup.string().required('Product description is required'),
+    category: Yup.string().required('The product category is required'),
+    image: Yup.mixed()
+      .required('Product image is required')
+      .test('fileType', 'The file must be an image', function (value: any) {
+        if (!value) return true;
+        const fileType = value && value.type;
+        const validFileTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+        if (fileType) {
+          return validFileTypes.includes(fileType);
+        }
+        return false;
+      }),
+  });
 
-  if (image instanceof File) {
-    // Convertir el objeto File a una URL o cadena base64
-    const reader = new FileReader();
-    reader.readAsDataURL(image);
-    reader.onloadend = () => {
-      productImage = reader.result as string;
-      createProduct(_id, title, price, description, category, productImage);
-    };
-  } else {
-    createProduct(_id, title, price, description, category, productImage);
-  }
-  //reestablecer los valores del formulario
-  resetForm()
-};
+  const handleSubmit = async (values: ProductForm, { resetForm }: FormikHelpers<ProductForm>) => {
+    console.log('submitting form:', values);
+    const { _id, title, price, description, category, image } = values;
 
-const createProduct = async (
-  _id: string,
-  title: string,
-  price: string,
-  description: string,
-  category: string,
-  image: string | null
-) => {
-  const newProduct: Product = {
-    _id,
-    title,
-    price,
-    description,
-    category,
-    image: image || '', // Si image es null, asignar una cadena vacía
-    rating: null
+    if (image instanceof File) {
+      try {
+        const formData = new FormData();
+        formData.append('file', image);
+        formData.append('upload_preset', 'Pilcha');
+
+        const response = await fetch(`https://api.cloudinary.com/v1_1/djwbjv70t/upload`, {
+          method: 'POST',
+          body: formData,
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          const imageUrl = data.secure_url;
+
+          createProduct(_id, title, price, description, category, imageUrl);
+        } else {
+          throw new Error('Image upload failed');
+        }
+      } catch (error) {
+        console.error('Error uploading image:', error);
+
+        // Mostrar mensaje de error utilizando el componente Toast
+        toast.error('Error al cargar la imagen. Por favor, inténtalo de nuevo.');
+      }
+    } else {
+      createProduct(_id, title, price, description, category, null);
+    }
+
+    // Restablecer los valores del formulario
+    resetForm();
   };
 
-  postProduct(newProduct)
-    .unwrap()
-    .then((response) => {
-      console.log('Product created:', response);
-      dispatch(setProducts([response]));
-      console.log('Updated products:', response);
-    })
-    .catch((error) => {
-      console.error('Error creating product:', error);
-    });
+  const createProduct = async (
+    _id: string,
+    title: string,
+    price: string,
+    description: string,
+    category: string,
+    image: string | null
+  ) => {
+    const newProduct: Product = {
+      _id,
+      title,
+      price,
+      description,
+      category,
+      image: image || '', // Si image es null, asignar una cadena vacía
+      rating: null,
     };
 
-    
-    
-    return (
-            <Formik
-            initialValues={{
-              _id: '',
-              title: '',
-              price: '',
-              description: '',
-              category: '',
-              image: null,
-              rating: null,
-            }}
-            validationSchema={validationSchema}
-            onSubmit={handleSubmit}
-          >
-            {({ setFieldValue }) => (
+    postProduct(newProduct)
+      .unwrap()
+      .then((response) => {
+        console.log('Product created:', response);
+        dispatch(setProducts([response]));
+        console.log('Updated products:', response);
 
-        <div className='flex justify-center items-center isolate bg-white px-6 py-24 sm:py-32 lg:px-8'>
-    <div className='flex justify-center'>
-      <div className='max-w-lg'>
-        <div className='p-6'>
-          <div className='mx-auto max-w-2xl text-center'>
-            <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl pb-10'>Cargar Producto</h2>
-          </div>
+        // Mostrar mensaje de éxito utilizando el componente Toast
+        toast.success('¡Producto cargado correctamente!');
+      })
+      .catch((error) => {
+        console.error('Error creating product:', error);
 
-          
-              <Form className='max-w-xl mx-auto' encType='multipart/form-data'>
+        // Mostrar mensaje de error utilizando el componente Toast
+        toast.error('Error al cargar el producto. Por favor, inténtalo de nuevo.');
+      });
+  };
+
+  return (
+    <Formik
+      initialValues={{
+        _id: '',
+        title: '',
+        price: '',
+        description: '',
+        category: '',
+        image: null,
+        rating: null,
+      }}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ setFieldValue }) => (
+        <div className="flex justify-center items-center isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+          <div className="flex justify-center">
+            <div className="max-w-lg">
+              <div className="p-6">
+                <div className="mx-auto max-w-2xl text-center">
+                  <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl pb-10">
+                    Cargar Producto
+                  </h2>
+                </div>
+
+                <Form className='max-w-xl mx-auto' encType='multipart/form-data'>
                 <div className='grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2'>
                   <div>
                     <label className='block text-sm font-semibold leading-6 text-gray-900' htmlFor='title'>
@@ -208,11 +233,14 @@ const createProduct = async (
                   </button>
                 </div>
               </Form>
+
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </div>
-            )}
-    </Formik>) }
+      )}
+    </Formik>
+  );
+};
 
 export default ProductForm;
