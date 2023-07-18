a72848e29a8814b8d09913dcc0f854f230372604
arielbloise <arielnbloise@gmail.com>
2023-07-18 19:24:48 -0300

    mejoras en el navbar y creada ruta y componente de admin

---
+++ client/app/admin/page.tsx
@@ -0,0 +1,11 @@
+import AdminDashboard from "components/AdminDashboard";
+
+const AdminPage = () => {
+    return (
+        <div>
+            <AdminDashboard/>
+        </div>
+    )
+}
+
+export default AdminPage;
