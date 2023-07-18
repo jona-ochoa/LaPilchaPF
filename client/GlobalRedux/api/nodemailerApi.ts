eeaf09d6d9dbfc6873efa188fa392e47f9956173
jochoa <jonatan.c.ochoa@gmail.com>
2023-07-18 19:24:34 -0300

    agregando nodemailer al formulario de contacto no está terminado

---
+++ client/GlobalRedux/api/nodemailerApi.ts
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export type Mailer = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
+
+export const nodemailerApi = createApi({
+  reducerPath: "nodemailerApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3002",
+  }),
+  endpoints: (builder) => ({
+    createMailer: builder.mutation<Mailer, Partial<Mailer>>({
+      query: (newMailer) => ({
+        url: "/email/purchase",
+        method: "POST",
+        body: newMailer,
+      }),
+    }),
+  }),
+});
+
+export const { useCreateMailerMutation } = nodemailerApi;
