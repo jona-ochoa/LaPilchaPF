29ed7e17789589d689c42dc2a70cd58e4c84fc09
jochoa <jonatan.c.ochoa@gmail.com>
2023-07-19 16:17:59 -0300

    Terminado de el form contact y agregando el loading

--- client/components/FormContact.tsx
+++ client/components/FormContact.tsx
@@ -1,13 +1,18 @@
 "use client"
 import React, { useState } from "react";
 import {
-  Formik, Form, Field, ErrorMessage,
+  Formik,
+  Form,
+  Field,
+
+  ErrorMessage,
   FormikHelpers
 } from 'formik';
 import * as Yup from 'yup';
 import { useAppDispatch } from "GlobalRedux/store";
 import { useCreateMailerMutation, Mailer } from "GlobalRedux/api/nodemailerApi";
 import { addMailer } from "GlobalRedux/features/nodemailerSlice";
+import toast from 'react-hot-toast';
 
 interface IFormInputs {
   name: string;
@@ -22,10 +27,10 @@ const FormContact: React.FC = () => {
   const dispatch = useAppDispatch();
 
   const validationSchema = Yup.object().shape({
-    name: Yup.string().min(3, 'Too short').max(50, 'Too long').required('Name is required'),
-    email: Yup.string().email('Invalid email').required('Email is required'),
-    subject: Yup.string().min(3, 'Too short').max(50, 'Too long').required('Subject is required'),
-    message: Yup.string().min(6, 'Message must be at least 6 characters').max(200, 'Too long').required('Email is required')
+    name: Yup.string().min(3, 'Mínimo 3 caracteres.').max(50, 'Maximo 50 caracteres.').required('Nombre es requerido.'),
+    email: Yup.string().email('Email Inválido.').required('Email es requerido.'),
+    subject: Yup.string().min(3, 'Mínimo 3 caracteres.').max(50, 'Máximo 50 caracteres.').required('Asunto es requerido.'),
+    message: Yup.string().min(6, 'Mínimo 6 caracteres.').max(200, 'Máximo 200 caracteres.').required('Mensaje es requerido.')
   })
 
   const handleSubmitMailer = async (values: IFormInputs, { resetForm }: FormikHelpers<IFormInputs>) => {
@@ -50,69 +55,78 @@ const FormContact: React.FC = () => {
       if ('data' in result) {
         const { data } = result;
         const jsonData = JSON.stringify(data);
-        console.log("Data de nodemailer: ", jsonData)
         dispatch(addMailer(data))
+        toast.success('Mensaje enviado con éxito', {
+          duration: 4000
+        });
+        window.location.href = '/login';
       } else {
-        console.log("Error en creating mailer")
+        console.log("Error al enviar el formulario.")
+        toast.error('Error de envio.');
       }
     } catch (error) {
       console.log(error?.message || "Error desconocido")
+      toast.error('Error al enviar el mensaje.');
     }
     resetForm();
   }
 
-
-
   return (
-    <div className="w-full h-full bg-gray-500 text-gray-900 p-10">
-      <Formik initialValues={{
-        name: '',
-        email: '',
-        subject: '',
-        message: '',
-      }} validationSchema={validationSchema} onSubmit={handleSubmitMailer}>
-        <Form
-          // onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))} 
-          className="max-w-xl mx-auto p-4 md:p-6 my-0 bg-gray-800 rounded-lg shadow-md"
+    <div className="w-full h-full pb-12 text-center bg-gray-500 text-gray-900 flex flex-col justify-center">
+      <div className="px-auto py-10">
+        <h2 className="text-gray-200 text-5xl text-center font-extrabold">Formulario de contacto</h2>
+      </div>
+      <div>
+        <Formik initialValues={{
+          name: '',
+          email: '',
+          subject: '',
+          message: '',
+        }}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmitMailer}
         >
+          <Form
+            className="max-w-xl mx-auto p-4 md:p-6 my-0 text-left bg-gray-800 rounded-lg shadow-md"
+          >
 
-          <ErrorMessage name="name" component="div" className="text-red-500" />
-          <Field
-            id="name"
-            name="name"
-            type="text"
-            placeholder="Nombre"
-            className="w-full px-3 py-2 mb-3 text-gray-900 border-white border rounded-lg focus:outline-none" />
+            <label htmlFor="name" className="text-white font-extrabold tracking-widest mb-2">Nombre</label>
+            <ErrorMessage name="name" component="div" className="text-red-500" />
+            <Field
+              id="name"
+              name="name"
+              type="text"
+              placeholder="Nombre"
+              className="w-full px-3 py-2 mb-3 text-gray-900 border-white border rounded-lg focus:outline-none" />
 
-          <ErrorMessage name="email" component="div" className="text-red-500" />
-          <Field
-            id="email"
-            name="email"
-            type="email"
-            placeholder="Email" className="w-full px-3 py-2 mb-3 text-gray-900 border-white border rounded-lg focus:outline-none" />
+            <label htmlFor="email" className="text-white font-extrabold tracking-widest mb-2">Email</label>
+            <ErrorMessage name="email" component="div" className="text-red-500" />
+            <Field
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Email" className="w-full px-3 py-2 mb-3 text-gray-900 border-white border rounded-lg focus:outline-none" />
 
-          <ErrorMessage name="subject" component="div" className="text-red-500" />
-          <Field
-            id="subject"
-            name="subject"
-            type="text"
-            placeholder="Asunto" className="w-full px-3 py-2 mb-3 text-gray-900 border-white border rounded-lg focus:outline-none" />
+            <label htmlFor="subject" className="text-white font-extrabold tracking-widest mb-2">Asunto</label>
+            <ErrorMessage name="subject" component="div" className="text-red-500" />
+            <Field
+              id="subject"
+              name="subject"
+              type="text"
+              placeholder="Asunto" className="w-full px-3 py-2 mb-3 text-gray-900 border-white border rounded-lg focus:outline-none" />
 
-          <ErrorMessage name="message" component="div" className="text-red-500" />
-          <Field
-            id="message"
-            name="message"
-            type="text"
-            placeholder="Deja Tú Mensaje" className="w-full px-3 py-2 mb-3 text-gray-900 border-white border rounded-lg focus:outline-none" />
-
-          {/* <p className="mb-3 text-gray-700">{data}</p> */}
-
-          <button type="submit"
-            // disabled={!isValid} 
-            // className={`w-full py-2 px-3 text-white rounded-lg font-medium tracking-wide transition duration-200 ${isValid ? 'bg-indigo-500 hover:bg-indigo-600 cursor-pointer' : 'bg-indigo-300 cursor-not-allowed'}`} />
-            className={`w-full py-2 px-3 text-white rounded-lg font-medium tracking-wide transition duration-200 bg-indigo-500 hover:bg-indigo-600 cursor-pointer'}`}>Enviar</button>
-        </Form>
-      </Formik>
+            <label htmlFor="message" className="text-white font-extrabold tracking-widest mb-2">Mensaje</label>
+            <ErrorMessage name="message" component="div" className="text-red-500" />
+            <Field
+              id="message"
+              as="textarea"
+              name="message"
+              placeholder="Deja Tú Mensaje" className="w-full px-3 py-2 mb-3 text-gray-900 border-white border rounded-lg focus:outline-none" />
+            <button type="submit"
+              className={`w-full py-2 px-3 text-white rounded-lg font-medium tracking-wide transition duration-200 bg-indigo-500 hover:bg-indigo-600 cursor-pointer'}`}>Enviar</button>
+          </Form>
+        </Formik>
+      </div>
     </div>
   );
 }
--- client/components/ProductList.tsx
+++ client/components/ProductList.tsx
@@ -101,38 +101,48 @@ const ProductList: React.FC = () => {
 
   return (
     <div>
-      <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 xl:gap-y-10">
-        {currentProducts.map((product) => (
-          <div key={product._id} className="product-card relative">
-            <ProductCard product={product} />
-            <div className="absolute top-2 right-7 flex flex-col bg-gray-200 p-1 rounded-sm">
-              <button onClick={() => handleFavorite(product)} className="">
-                {favoriteItems.find((item) => item._id === product._id) ? (
-                  <MdFavorite className="w-[25px] h-[25px] text-red-500 transition-transform" />
-                ) : (
-                  <MdFavoriteBorder className="w-[25px] h-[25px] text-gray-800 transition-transform opacity-75" />
-                )}
-              </button>
-              <button onClick={() => handleCartItem(product)} className="">
-                {cartItems.find((item) => item._id === product._id) ? (
-                  <BsFillCartCheckFill className="w-[25px] h-[25px] text-green-500 transition-transform" />
-                ) : (
-                  <BsFillCartDashFill className="w-[25px] h-[25px] text-gray-800 transition-transform opacity-75" />
-                )}
-              </button>
-            </div>
-          </div>
-        ))}
-      </div>
+      {isLoading ? (
 
+        <div role="status" className="flex flex-col text-center justify-center">
+          <svg aria-hidden="true" className="w-[100px] h-[100px] mx-auto text-center text-gray-900 animate-spin dark:text-gray-200 fill-gray-900" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+          </svg>
+          <h3 className="text-gray-900 font-bold text-5xl my-5">Loading...</h3>
+        </div>
+
+      ) : (
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 xl:gap-y-10">
+          {currentProducts.map((product) => (
+            <div key={product._id} className="product-card relative">
+              <ProductCard product={product} />
+              <div className="absolute top-2 right-7 flex flex-col bg-gray-200 p-1 rounded-sm">
+                <button onClick={() => handleFavorite(product)} className="">
+                  {favoriteItems.find((item) => item._id === product._id) ? (
+                    <MdFavorite className="w-[25px] h-[25px] text-red-500 transition-transform" />
+                  ) : (
+                    <MdFavoriteBorder className="w-[25px] h-[25px] text-gray-800 transition-transform opacity-75" />
+                  )}
+                </button>
+                <button onClick={() => handleCartItem(product)} className="">
+                  {cartItems.find((item) => item._id === product._id) ? (
+                    <BsFillCartCheckFill className="w-[25px] h-[25px] text-green-500 transition-transform" />
+                  ) : (
+                    <BsFillCartDashFill className="w-[25px] h-[25px] text-gray-800 transition-transform opacity-75" />
+                  )}
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
       <div className="flex justify-center mt-6">
         {Array.from({ length: totalPages }, (_, index) => (
           <button
             key={index}
             onClick={() => handlePageChange(index + 1)}
-            className={`px-3 py-1 mx-1 ${
-              currentPage === index + 1 ? 'bg-gray-600 text-white' : 'bg-gray-300 text-gray-700'
-            }`}
+            className={`px-3 py-1 mx-1 ${currentPage === index + 1 ? 'bg-gray-600 text-white' : 'bg-gray-300 text-gray-700'
+              }`}
           >
             {index + 1}
           </button>
