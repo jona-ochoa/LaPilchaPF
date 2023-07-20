d8fe1018c38f52ac3adfc2bdfd3e59943c859832
AleSaturno <alesaturno64@gmail.com>
2023-07-20 11:09:35 -0300

    Nueva estructura del about

--- client/components/AboutCard.tsx
+++ client/components/AboutCard.tsx
@@ -1,43 +1,128 @@
-import React from "react"
+import React from "react";
 
+interface Member {
+  name: string;
+  rol: string;
+  github: string;
+  linkedin: string;
+}
+
+const members: Member[] = [
+    {
+        name: "Gonzalo Sebastián Lagioia",
+        rol: "PF-Mentor",
+        github: "https://github.com/glagioia",
+        linkedin: "https://www.linkedin.com/in/gonzalo-sebastian-lagioia/"
+    },
+    {
+        name: "Jonatan Cristian Ochoa",
+        rol:"Back-End & Front-End",
+        github: "https://github.com/jona-ochoa",
+        linkedin: "https://linkedIn.com/in/jonaochoa",
+    },
+    {
+        name: "Alejo Ivan Holmann",
+        rol: "Back-End & Front-End",
+        github: "https://github.com/AIHolmann",
+        linkedin: "https://www.linkedin.com/in/alejo-holmann-a51262221/",
+    },
+    {
+        name: "Tomas Balmaceda",
+        rol: "Back-End",
+        github: "https://github.com/tomasbalmaceda",
+        linkedin: "https://www.linkedin.com/in/tom%C3%A1s-balmaceda-357328234/",
+    },
+    {
+        name: "Ariel Nicolas Bloise",
+        rol: "Front-End & Back-End",
+        github: "https://github.com/arielbloise",
+        linkedin: "https://www.linkedin.com/in/ariel-bloise/",
+    },
+    {
+        name: "Malena Araceli Valdez",
+        rol: "Front-End",
+        github: "https://github.com/malevaldezok",
+        linkedin: "https://www.linkedin.com/mwlite/in/malena-valdez-488a52281 ",
+    },
+    {
+        name: "Alejandro Saturno",
+        rol: "Front-End",
+        github: "https://github.com/AleSaturno",
+        linkedin: "https://www.linkedin.com/in/alejandro-saturno-1b5967206/",
+    },
+    {
+        name: "Julian Maria Hary Beccar Varela",
+        rol:"Front-End",
+        github: "Https://gitHub.com/JMHaryBeccarVarela",
+        linkedin: "Https://linkedin.com/in/julian-hary-beccar-varela",
+    },
+];
 
 const AboutCard: React.FC = () => {
-    return (
-<div className="w-96 px-6 py-6  text-center bg-gray-800 rounded-lg lg:mt-0 xl:px-10">
-    <div className="space-y-4 xl:space-y-6">
-    <img
-          className="mx-auto rounded-full h-36 w-36"
-          src="https://thispersondoesnotexist.com/image"
-          alt="author avatar"
-        />
-        <div className="space-y-2">
-            <div className="flex justify-center items-center flex-col space-y-3 text-lg font-medium leading-6">
-                <h3 className="text-white">John Doe</h3>
-                <p className="text-indigo-300">Web Developer</p>
-                <div className="flex justify-center mt-5 space-x-5"><a href="#" target="_blank" rel="noopener noreferrer"
-                        className="inline-block text-gray-400"><span className="sr-only">GitHub</span><svg stroke="currentColor"
-                            fill="currentColor" stroke-width="0" viewBox="0 0 496 512"
-                            className="w-6 h-6 text-gray-400 hover:text-gray-100" height="1em" width="1em"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z">
-                            </path>
-                        </svg></a>
-                    <a href="#" target="_blank" rel="noopener noreferrer"
-                        className="inline-block text-gray-400"><span className="sr-only">Linkedin</span><svg
-                            stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512"
-                            className="w-6 h-6 text-gray-400 hover:text-gray-100" height="1em" width="1em"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z">
-                            </path>
-                        </svg></a>
+  return (
+    <div className="grid grid-cols-1 gap-6 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4">
+      {members.map((member) => {
+        const githubUsername = member.github.split("/").pop() || "";
+
+        return (
+          <div
+            key={member.name}
+            className="w-90 px-8 py-6 text-center bg-gray-800 rounded-lg"
+          >
+            <img
+              className="mx-auto rounded-full h-36 w-36"
+              src={`https://github.com/${githubUsername}.png`}
+              alt={member.name}
+            />
+            <div className="space-y-2">
+              <div className="flex justify-center items-center flex-col space-y-3 text-lg font-medium leading-6">
+                <h3 className="text-white">{member.name}</h3>
+                <h3 className="text-white">{member.rol}</h3>
+                <div className="flex justify-center mt-5 space-x-5">
+                  <a
+                    href={member.github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-block text-gray-400"
+                  >
+                    <span className="sr-only">GitHub</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                      className="w-6 h-6 text-gray-400 hover:text-gray-100"
+                    >
+                      <path
+                        d="M12 2C6.477 2 2 6.477 2 12c0 4.418 2.865 8.166 6.839 9.489.5.092.682-.217.682-.482 0-.237-.009-.868-.014-1.703-2.782.602-3.369-1.34-3.369-1.34-.454-1.154-1.11-1.46-1.11-1.46-.908-.62.069-.608.069-.608 1.004.07 1.532 1.03 1.532 1.03.892 1.529 2.341 1.089 2.91.833.092-.646.349-1.089.636-1.34-2.22-.25-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.03-2.682-.103-.251-.447-1.27.098-2.645 0 0 .84-.27 2.75 1.025A9.564 9.564 0 0112 7.33c.852.004 1.706.114 2.504.334 1.909-1.294 2.747-1.025 2.747-1.025.546 1.375.202 2.394.1 2.645.64.698 1.027 1.59 1.027 2.682 0 3.841-2.337 4.688-4.565 4.937.36.31.68.92.68 1.852 0 1.336-.013 2.415-.013 2.744 0 .267.18.578.688.48A10.019 10.019 0 0022 12c0-5.523-4.477-10-10-10z"
+                      />
+                    </svg>
+                  </a>
+                  <a
+                    href={member.linkedin}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-block text-gray-400"
+                  >
+                    <span className="sr-only">Linkedin</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 448 512"
+                      className="w-6 h-6 text-gray-400 hover:text-gray-100" height="1em" width="1em"
+                    >
+                      <path
+                        d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"
+                      />
+                    </svg>
+                  </a>
                 </div>
+              </div>
             </div>
-        </div>
+          </div>
+        );
+      })}
     </div>
-</div>
-)
-}
+  );
+};
 
 export default AboutCard;
