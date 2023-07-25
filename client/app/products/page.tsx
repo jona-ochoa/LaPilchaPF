cd2ce71bc1ad7a21d19bfb3cfdc1b7f686f34ebe
JMHaryBeccarVarela <123906403+JMHaryBeccarVarela@users.noreply.github.com>
2023-07-25 18:29:25 -0300

    ADD- filtros y productos responsive + accessdenied

--- client/app/products/page.tsx
+++ client/app/products/page.tsx
@@ -1,20 +1,44 @@
 "use client"
-
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductList from "../../components/ProductList";
 import ProductFilter from "components/ProductFilter";
-
+import { useDispatch } from "react-redux";
+import { useSession } from "next-auth/react";
+import { User } from "../../GlobalRedux/api/usersApi";
+import { setUsers } from "GlobalRedux/features/usersSlice";
+import { useGetUsersQuery } from "../../GlobalRedux/api/usersApi";
+import AccessDenied from "../../components/AccessDenied";
 
 const ProductsPage = () => {
-    return (
-        <div className="mx-auto max-w-2xl px-4 py-10 sm:px-6 sm:py-10 lg:max-w-7xl lg:px-8" >
-        <div className="bg-gray-100 p-2 pt-10 m-5">
-  <h1 className="text-6xl font-bold text-center mb-8 bg-gray-900 text-white rounded-full py-2 px-4">Productos Destacados</h1> 
-</div>
-            <ProductFilter/>
-            <ProductList/>
+  const dispatch = useDispatch();
+  const { data, error, isLoading, isFetching } = useGetUsersQuery();
+  const [usersData, setUsersData] = useState<User[]>([]);
+  const { data: sessionData } = useSession();
+
+  useEffect(() => {
+    if (data) {
+      const users = data as User[];
+      setUsersData(users);
+      dispatch(setUsers(users));
+    }
+  }, [data, dispatch]);
+
+  const currentUser = usersData.find((user) => user.email === sessionData?.user?.email);
+
+  return (
+    <div>
+      {currentUser && currentUser.isBanned ? (
+        <AccessDenied />
+      ) : (
+        <div className="mx-auto max-w-2xl px-4 py-10 sm:px-6 sm:py-10 lg:max-w-7xl lg:px-8">
+          <ProductFilter />
+          <ProductList />
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
 export default ProductsPage;
+
+
