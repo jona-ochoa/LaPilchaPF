edc6381e38b471d59148f6affc75a150bdc9d78d
JMHaryBeccarVarela <123906403+JMHaryBeccarVarela@users.noreply.github.com>
2023-07-26 13:00:31 -0300

    fix- borre el punto rojo del carrito

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -2,17 +2,16 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import Image from 'next/image';
-import {RootState} from "../GlobalRedux/store"
 import Link from 'next/link';
 import { useSession, signOut } from 'next-auth/react';
+import { RootState } from "../GlobalRedux/store"
 import { useSelector } from 'react-redux';
 import { MdOutlineAdminPanelSettings } from 'react-icons/md';
 import { User, useGetUsersQuery } from 'GlobalRedux/api/usersApi';
 
+
 const Navbar: React.FC = () => {
   const {data: userList} = useGetUsersQuery()
- 
-  console.log(userList);
 
   const { data: session, status } = useSession();
   const isAdminUser = userList?.find((user) => user.email === session?.user?.email)?.isAdmin;
@@ -138,30 +137,27 @@ const Navbar: React.FC = () => {
               </div>
             </Link>
             <Link href="/carrito">
-              <div>
-                <div className="flex items-center hover:text-gray-200">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                    />
-                  </svg>
+  <div>
+    <div className="flex items-center hover:text-gray-200">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+        />
+      </svg>
+     
+    </div>
+  </div>
+</Link>
 
-                  <span className="flex absolute -mt-5 ml-4">
-                    <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
-                    <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500"></span>
-                  </span>
-                </div>
-              </div>
-            </Link>
           </div>
           {renderUserIcon()}
         </div>
