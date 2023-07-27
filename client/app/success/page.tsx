d91058d63af60958896da991cbede42970693edb
arielbloise <arielnbloise@gmail.com>
2023-07-27 02:00:28 -0300

    arreglos menores

--- client/app/success/page.tsx
+++ client/app/success/page.tsx
@@ -139,7 +139,7 @@ const ThankYouPage = () => {
   console.log(orderCreated)
   
   return (
-    <div className="flex flex-col justify-center justify-items-center py-1.5 ">
+    <div className="flex flex-col justify-center justify-items-center py-1.5 bg-cyan-900">
       <div className="flex flex-row flex-wrap justify-center justify-items-center mx-2.5">
         <div       
           className="w-1/4"
